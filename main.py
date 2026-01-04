6fbb1734b9fddeacfe72c0feea370d086ea71ff0
PauSGracia <pau.sole@gracia.lasalle.cat>
2026-01-04 16:31:12 +0100

    feat: finish menu

--- main.py
+++ main.py
@@ -16,6 +16,28 @@ def on_left_pressed():
         False)
 controller.left.on_event(ControllerButtonEvent.PRESSED, on_left_pressed)
 
+def start_menu():
+    global title, play, story, nena
+    scene.set_background_image(assets.image("""
+        menu_bg
+        """))
+    title = textsprite.create("TREASURE ESCAPE", 0, 2)
+    title.set_max_font_height(9)
+    play = textsprite.create("JUGAR")
+    story = textsprite.create("HISTORIA")
+    title.set_position(80, 10)
+    play.set_position(30, 90)
+    story.set_position(120, 90)
+    play.set_kind(SpriteKind.text)
+    story.set_kind(SpriteKind.text)
+    play.set_flag(SpriteFlag.GHOST, False)
+    story.set_flag(SpriteFlag.GHOST, False)
+    nena = sprites.create(assets.image("""
+        nena-front
+        """), SpriteKind.player)
+    controller.move_sprite(nena)
+    nena.set_stay_in_screen(True)
+
 def on_right_pressed():
     animation.run_image_animation(nena,
         assets.animation("""
@@ -34,8 +56,12 @@ def on_down_pressed():
         False)
 controller.down.on_event(ControllerButtonEvent.PRESSED, on_down_pressed)
 
+def on_on_overlap(sprite, otherSprite):
+    nena.say_text(":)")
+sprites.on_overlap(SpriteKind.player, SpriteKind.text, on_on_overlap)
+
+story: TextSprite = None
+play: TextSprite = None
+title: TextSprite = None
 nena: Sprite = None
-nena = sprites.create(assets.image("""
-    nena-front
-    """), SpriteKind.player)
-controller.move_sprite(nena)
+start_menu()
--- main.ts
+++ main.ts
@@ -14,6 +14,23 @@ controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
     false
     )
 })
+function start_menu () {
+    scene.setBackgroundImage(assets.image`menu_bg`)
+    title = textsprite.create("TREASURE ESCAPE", 0, 2)
+    title.setMaxFontHeight(9)
+    play = textsprite.create("JUGAR")
+    story = textsprite.create("HISTORIA")
+    title.setPosition(80, 10)
+    play.setPosition(30, 90)
+    story.setPosition(120, 90)
+    play.setKind(SpriteKind.Text)
+    story.setKind(SpriteKind.Text)
+    play.setFlag(SpriteFlag.Ghost, false)
+    story.setFlag(SpriteFlag.Ghost, false)
+    nena = sprites.create(assets.image`nena-front`, SpriteKind.Player)
+    controller.moveSprite(nena)
+    nena.setStayInScreen(true)
+}
 controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
     animation.runImageAnimation(
     nena,
@@ -30,6 +47,15 @@ controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
     false
     )
 })
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Text, function (sprite, otherSprite) {
+    if (otherSprite == play) {
+        sprite.sayText("Prem A per jugar", 100, false)
+    } else {
+        sprite.sayText("Prem A per veure la historia", 100, false)
+    }
+})
+let story: TextSprite = null
+let play: TextSprite = null
+let title: TextSprite = null
 let nena: Sprite = null
-nena = sprites.create(assets.image`nena-front`, SpriteKind.Player)
-controller.moveSprite(nena)
+start_menu()
