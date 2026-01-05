9bcf7c2f468dd5690f4b029738cc695b04838c91
PauSGracia <pau.sole@gracia.lasalle.cat>
2026-01-05 14:36:37 +0100

    feat: core game

--- main.py
+++ main.py
@@ -7,6 +7,20 @@ def on_up_pressed():
         False)
 controller.up.on_event(ControllerButtonEvent.PRESSED, on_up_pressed)
 
+def on_a_pressed():
+    if in_game == 1:
+        nena.vy = -150
+controller.A.on_event(ControllerButtonEvent.PRESSED, on_a_pressed)
+
+def on_on_overlap(sprite2, otherSprite2):
+    if otherSprite2 == play:
+        sprite2.say_text("Prem A per jugar", 100, False)
+    else:
+        sprite2.say_text("Prem A per veure la historia", 100, False)
+        if controller.A.is_pressed():
+            start_story()
+sprites.on_overlap(SpriteKind.player, SpriteKind.text, on_on_overlap)
+
 def on_left_pressed():
     animation.run_image_animation(nena,
         assets.animation("""
@@ -17,7 +31,8 @@ def on_left_pressed():
 controller.left.on_event(ControllerButtonEvent.PRESSED, on_left_pressed)
 
 def start_menu():
-    global title, play, story, nena
+    global in_game, title, play, story, nena
+    in_game = 0
     scene.set_background_image(assets.image("""
         menu_bg
         """))
@@ -37,6 +52,25 @@ def start_menu():
         """), SpriteKind.player)
     controller.move_sprite(nena)
     nena.set_stay_in_screen(True)
+def prepare_transition():
+    title.destroy()
+    play.destroy()
+    story.destroy()
+    sprites.destroy(nena)
+def start_game():
+    global in_game, nena
+    in_game = 1
+    scene.set_background_image(assets.image("""
+        game_bg
+        """))
+    info.set_life(2)
+    info.set_score(0)
+    nena = sprites.create(assets.image("""
+        nena-front
+        """), SpriteKind.player)
+    nena.set_position(20, ground_y)
+    nena.ay = 350
+    nena.set_stay_in_screen(True)
 
 def on_right_pressed():
     animation.run_image_animation(nena,
@@ -48,10 +82,20 @@ def on_right_pressed():
 controller.right.on_event(ControllerButtonEvent.PRESSED, on_right_pressed)
 
 def start_story():
-    title.destroy()
-    play.destroy()
-    story.destroy()
-    nena.set_flag(SpriteFlag.INVISIBLE, True)
+    prepare_transition()
+    scene.set_background_image(assets.image("""
+        placeholder1
+        """))
+    game.show_long_text("TEXT 1", DialogLayout.BOTTOM)
+    scene.set_background_image(assets.image("""
+        placeholder2
+        """))
+    game.show_long_text("TEXT 2", DialogLayout.BOTTOM)
+    scene.set_background_image(assets.image("""
+        palceholder3
+        """))
+    game.show_long_text("TEXT 3", DialogLayout.BOTTOM)
+    start_menu()
 
 def on_down_pressed():
     animation.run_image_animation(nena,
@@ -62,15 +106,58 @@ def on_down_pressed():
         False)
 controller.down.on_event(ControllerButtonEvent.PRESSED, on_down_pressed)
 
-def on_on_overlap(sprite, otherSprite):
-    if otherSprite == play:
-        sprite.say_text("Prem A per jugar", 100, False)
-    else:
-        sprite.say_text("Prem A per veure la historia", 100, False)
-sprites.on_overlap(SpriteKind.player, SpriteKind.text, on_on_overlap)
+# Visual feedback
 
+def on_on_overlap2(sprite, otherSprite):
+    otherSprite.destroy()
+    info.change_life_by(-1)
+    scene.camera_shake(4, 500)
+sprites.on_overlap(SpriteKind.player, SpriteKind.enemy, on_on_overlap2)
+
+obstacle2: Sprite = None
+random_obstacle = 0
 story: TextSprite = None
-play: TextSprite = None
 title: TextSprite = None
+play: TextSprite = None
+in_game = 0
 nena: Sprite = None
+ground_y = 0
+obstacle = None
+ground_y = 100
 start_menu()
+
+def on_on_update():
+    if in_game != 1:
+        return
+
+    info.change_score_by(1)
+    if info.score() >= 10000:
+        game.over(True)
+    if nena.y > ground_y:
+        nena.y = ground_y
+        nena.vy = 0
+game.on_update(on_on_update)
+
+def on_update_interval():
+    if in_game != 1:
+            return
+            
+    global random_obstacle, obstacle2
+    random_obstacle = randint(1, 3)
+    if random_obstacle == 1:
+        obs_img = assets.image("""
+            obstacle_1
+            """)
+    elif random_obstacle == 2:
+        obs_img = assets.image("""
+            obstacle_2
+            """)
+    else:
+        obs_img = assets.image("""
+            obstacle_3
+            """)
+    obstacle2 = sprites.create(obs_img, SpriteKind.enemy)
+    obstacle2.set_position(160, ground_y)
+    obstacle2.vx = -80
+    obstacle2.set_flag(SpriteFlag.AUTO_DESTROY, True)
+game.on_update_interval(1500, on_update_interval)
--- main.ts
+++ main.ts
@@ -6,6 +6,24 @@ controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
     false
     )
 })
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (in_game == 1 && nena.y >= ground_y) {
+        nena.vy = -200
+    }
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Text, function (sprite2, otherSprite2) {
+    if (otherSprite2 == play) {
+        sprite2.sayText("Prem A per jugar", 100, false)
+        if (controller.A.isPressed()) {
+            start_game()
+        }
+    } else {
+        sprite2.sayText("Prem A per veure la historia", 100, false)
+        if (controller.A.isPressed()) {
+            start_story()
+        }
+    }
+})
 controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
     animation.runImageAnimation(
     nena,
@@ -15,6 +33,7 @@ controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
     )
 })
 function start_menu () {
+    in_game = 0
     scene.setBackgroundImage(assets.image`menu_bg`)
     title = textsprite.create("TREASURE ESCAPE", 0, 2)
     title.setMaxFontHeight(9)
@@ -31,6 +50,23 @@ function start_menu () {
     controller.moveSprite(nena)
     nena.setStayInScreen(true)
 }
+function prepare_transition () {
+    title.destroy()
+    play.destroy()
+    story.destroy()
+    sprites.destroy(nena)
+}
+function start_game () {
+    prepare_transition()
+    in_game = 1
+    scene.setBackgroundImage(assets.image`game_bg`)
+    info.setLife(2)
+    info.setScore(0)
+    nena = sprites.create(assets.image`nena-front`, SpriteKind.Player)
+    nena.setPosition(20, ground_y)
+    nena.ay = 350
+    nena.setStayInScreen(true)
+}
 controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
     animation.runImageAnimation(
     nena,
@@ -40,10 +76,7 @@ controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
     )
 })
 function start_story () {
-    title.destroy()
-    play.destroy()
-    story.destroy()
-    sprites.destroy(nena)
+    prepare_transition()
     scene.setBackgroundImage(assets.image`placeholder1`)
     game.showLongText("TEXT 1", DialogLayout.Bottom)
     scene.setBackgroundImage(assets.image`placeholder2`)
@@ -60,18 +93,51 @@ controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
     false
     )
 })
-sprites.onOverlap(SpriteKind.Player, SpriteKind.Text, function (sprite, otherSprite) {
-    if (otherSprite == play) {
-        sprite.sayText("Prem A per jugar", 100, false)
-    } else {
-        sprite.sayText("Prem A per veure la historia", 100, false)
-        if (controller.A.isPressed()) {
-            start_story()
-        }
+// Visual feedback
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
+    otherSprite.destroy()
+    info.changeLifeBy(-1)
+    scene.cameraShake(4, 500)
+})
+let obstacle2: Sprite = null
+let random_obstacle = 0
+let story: TextSprite = null
+let title: TextSprite = null
+let play: TextSprite = null
+let in_game = 0
+let nena: Sprite = null
+let ground_y = 0
+let obstacle = null
+ground_y = 100
+start_menu()
+game.onUpdate(function () {
+    if (in_game != 1) {
+        return
+    }
+    info.changeScoreBy(1)
+    if (info.score() >= 10000) {
+        game.over(true)
+    }
+    if (nena.y > ground_y) {
+        nena.y = ground_y
+        nena.vy = 0
     }
 })
-let story: TextSprite = null
-let play: TextSprite = null
-let title: TextSprite = null
-let nena: Sprite = null
-start_menu()
+game.onUpdateInterval(1500, function () {
+    let obs_img: Image;
+if (in_game != 1) {
+        return
+    }
+    random_obstacle = randint(1, 3)
+    if (random_obstacle == 1) {
+        obs_img = assets.image`obstacle_1`
+    } else if (random_obstacle == 2) {
+        obs_img = assets.image`obstacle_2`
+    } else {
+        obs_img = assets.image`obstacle_3`
+    }
+    obstacle2 = sprites.create(obs_img, SpriteKind.Enemy)
+    obstacle2.setPosition(160, ground_y)
+    obstacle2.vx = -80
+    obstacle2.setFlag(SpriteFlag.AutoDestroy, true)
+})
